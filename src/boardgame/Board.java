5fd4b624586c0b3f8b5d156e195f66fca90fa86a
Gabriel Resende <gabriellrr2005@gmail.com>
2023-05-30 11:11:57 -0300

    Chess layer and priting the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,7 +28,13 @@ public class Board {
 		this.colums = colums;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 	
 }
